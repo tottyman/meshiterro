ba2ffbca2ba0d55726bd46a76b479ace8fb70b6d
ToshiyukiIsshiki <114905631+tottyman@users.noreply.github.com>
2022-11-10 10:20:09 +0000

    [Finish] meshiterro

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
+  get "/homes/about" => "homes#about" ,as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, onry: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+
+end
